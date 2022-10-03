67fbd89f4a0d56978b4f99247ec0d8669fbae6f7
Shubham vyas <112367132+Shubhamvyas45@users.noreply.github.com>
2022-10-03 15:40:20 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,23 +1,25 @@
+publicclass Hello {
 
-  public class hello{
-    public static void pyramidPattern(int n) 
-    {  
-        for (int i=0; i<n; i++) //outer loop for number of rows(n) { for (int j=n-i; j>1; j--) //inner loop for spaces
-            { 
-                System.out.print(" "); //print space
-            }  
-            for (int j=0; j<=i; j++ ) //inner loop for number of columns
-            { 
-                System.out.print("* "); //print star
-            } 
-  
-            System.out.println(); //ending line after each row
-        } 
-    } 
-    
-    public static void main(String args[]) //driver function
-    { 
-        int n = 5; 
-        pyramidPattern(n); 
-    } 
+
+
+  publicstatic void main(String[] args) {
+
+    introws = 5;
+
+
+
+    for (inti = 1; i <= rows; ++i) {
+
+      for (intj = 1; j <= i; ++j) {
+
+        System.out.print("* ");
+
+      }
+
+      System.out.println();
+
+    }
+
+  }
+
 }
